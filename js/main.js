ee8e6ad72daeb8eb885ac04481005e23cb053c83
Thiago <thiagopazz1230@gmail.com>
2024-01-07 17:22:45 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 0000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#cep').mask('00000-000')
+
+    $('form').validate({
+        rules: {
+            nomeCompleto: {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+            
+        }
+        
+    })
+})
